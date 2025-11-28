3b5cee3abcc16277b4cab167b0921747930cc3ed
Santhosh Kumar <whitethoughtsdigitalleads@gmail.com>
2025-11-28 19:02:11 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -35,7 +35,7 @@ const Projects = () => {
         <img
           src={ResidentialVillas}
           alt="Sample"
-          className="w-full h-full object-cover rounded-3xl"
+          className="w-full h-full object-cover rounded-2xl"
         />
       </div>
 
@@ -53,7 +53,7 @@ const Projects = () => {
         <img
           src={PremiusCloubhouse}
           alt="Sample"
-          className="w-full h-full object-cover rounded-3xl"
+          className="w-full h-full object-cover"
         />
       </div>
       <div className="w-full flex justify-center pb-[50px] md:pb-[100px] bg-image">
