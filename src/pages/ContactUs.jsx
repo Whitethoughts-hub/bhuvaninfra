f5f2cc01c34c82e08a5d2020abeb091128d6982a
Santhosh Kumar <whitethoughtsdigitalleads@gmail.com>
2025-11-30 15:33:26 +0530

    Update ContactUs.jsx
    contact us page layout set

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,14 +1,19 @@
 import Contact from "../components/Contact/Contact";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+
 const ContactUs = () => {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Navbar />
 
-      <Contact />
+      {/* Main Scroll Content */}
+      <main className="flex-1">
+        <Contact />
+      </main>
+
       <Footer />
-    </>
+    </div>
   );
 };
 
